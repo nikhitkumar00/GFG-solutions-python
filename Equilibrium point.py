094b1e3203287e0e2e3f10fd4c187d2949efe5df
nikhitkumar <nikhitkumar00@gmail.com>
2023-08-10 22:44:21 +0530

    new

---
+++ Equilibrium point.py
@@ -0,0 +1,22 @@
+def equilibriumpoint(A,n):
+    
+    front = 0
+    back = 0
+    i = 0
+    j = n-1
+    while(i < j):
+        if front <= back:
+            front += A[i]
+            i += 1
+        else:
+            back += A[j]
+            j -= 1
+    if front == back:
+        return i + 1
+    else:
+        return -1
+
+
+c = "20 17 42 25 32 32 30 32 37 9 2 33 31 17 14 40 9 12 36 21 8 33 6 6 10 37 12 26 21 3"
+c = [int(x) for x in c.split()]
+print(equilibriumpoint(c,len(c)))
