143a62f22afe52d8340f335e72a45e6ceeed8710
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-08-13 19:19:58 +0530

    done

--- Largest number formed from an array.py
+++ Largest number formed from an array.py
@@ -4,11 +4,16 @@ class Solution:
         result = "".join(arr)
         return result if result[0] != "0" else "0"
 
+"""
+5555555555
+3333333333
+30303030303030303030
+34343434343434343434
+9999999999
+"""
 
-import functools
-
-n = int(input())
-arr = list(map(str, input().strip().split()))
+n = 5
+arr = list(map(str, "5 3 30 34 5 9".strip().split()))
 ob = Solution()
 ans = ob.printLargest(arr)
 print(ans)
