819aa382c7fad2688599bf8c0b707c4c6767946d
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-08-14 12:56:23 +0530

    incomplete

--- Subarray with given sum.py
+++ Subarray with given sum.py
@@ -1,19 +1,24 @@
-def subArraySum(a, n, s):
-    start = 0  # Start of the sliding window
-    current_sum = 0
+def subArraySum(arr, n, s):
+    current = 0
+    left = 0
+    right = 0
+    while right < n:
+        if current == s:
+            return [left + 1, right] if left < right else [-1]
+        if current < s:
+            current += arr[right]
+            right += 1
+        else:
+            current -= arr[left]
+            left += 1
+    if current == s:
+        return [left + 1, right] if left < right else [-1]
+    return [-1]
 
-    for end in range(n):
-        current_sum += a[end]
-
-        while current_sum > s:
-            current_sum -= a[start]
-            start += 1
-
-        if current_sum == s:
-            return start + 1, end + 1
-
-    return -1
 
 print(subArraySum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10, 15))
 print(subArraySum([1, 2, 3, 7, 5], 5, 12))
 print(subArraySum([1, 2, 3, 4], 4, 0))
+s = "135 101 170 125 79 159 163 65 106 146 82 28 162 92 196 143 28 37 192 5 103 154 93 183 22 117 119 96 48 127 172 139 70 113 68 100 36 95 104 12 123 134"
+l = [int(x) for x in s.split()]
+print(subArraySum(l, 42, 468))
