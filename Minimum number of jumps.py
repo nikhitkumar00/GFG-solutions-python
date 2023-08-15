a92b687322dfe2008607c0982c7006cdf0a02d77
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-08-15 19:36:32 +0530

    incomplete

---
+++ Minimum number of jumps.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minJumps(self, arr, n):
+        jump = 0
+        i = 0
+        if arr[0] == 0:
+            return -1
+        while i < n:
+            max = 0
+
+        return jump
+
+
+n = 10
+Arr = [2, 3, 1, 1, 2, 4, 2, 0, 1, 1]
+ob = Solution()
+ans = ob.minJumps(Arr, n)
+print(ans)
