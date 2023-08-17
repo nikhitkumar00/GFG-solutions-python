6cbc1587f8c53fe7243d7e034f9b1d3268d16506
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-08-18 00:40:36 +0530

    done

---
+++ Remaining string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def printString(self, S, ch, count):
+        temp = 0
+        if count == 0:
+            return S
+        for i in range(len(S)):
+            if S[i] == ch:
+                temp += 1
+                if temp == count:
+                    return S[i+1:] if len(S[i+1:])>0 else "Empty string"
+        return "Empty string"
+
+
+s = "ieiei"
+ch = "i"
+count = 3
+ob = Solution()
+answer = ob.printString(s, ch, count)
+print(answer)
