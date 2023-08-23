b2ca66e34df13434e253db4a66e7781f7347696b
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-08-23 19:17:52 +0530

    done

---
+++ Search in an rotated array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def search(self, A: list, l: int, h: int, key: int):
+        return A.index(key) if key in A else -1
+
+n = int(input())
+A = [int(x) for x in input().split()]
+k = int(input())
+ob = Solution()
+print(ob.search(A, 0, n - 1, k))
