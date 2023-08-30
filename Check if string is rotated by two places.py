c4ad748914b24e2dcb1571a1ebaed122ecd470e8
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-08-30 21:53:39 +0530

    done

---
+++ Check if string is rotated by two places.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isRotated(self, str1, str2):
+        new1 = str1[2:] + str1[0:2]
+        new2 = str1[-2:] + str1[:-2]
+        return 1 if str2 == new1 or str2 == new2 else 0
+
+
+s = "fsbcnuvqhffbsaqxwp"
+p = "wpfsbcnuvqhffbsaqx"
+if Solution().isRotated(s, p):
+    print(1)
+else:
+    print(0)
