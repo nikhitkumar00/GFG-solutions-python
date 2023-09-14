b54a78bf0c02c636ff65854b1b38dadacf30b83f
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-09-14 19:31:58 +0530

    done

---
+++ Longest distinct characters in a string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestSubstrDistinctChars(self, S):
+        left = 0
+        right = 0
+        dic = {}
+        maxx = -1
+        while right < len(S):
+            if S[right] in dic:
+                dic.pop(S[left],None)
+                left += 1
+            else:
+                dic[S[right]] = 1
+                right += 1
+            maxx = len(dic) if len(dic) > maxx else maxx
+        return maxx
+
+S = "aaa"
+solObj = Solution()
+ans = solObj.longestSubstrDistinctChars(S)
+print(ans)
