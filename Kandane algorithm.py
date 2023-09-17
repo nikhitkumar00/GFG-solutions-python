5acb3a733deec8114d062f1622cd1cc5cffa83ed
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-09-17 23:11:37 +0530

    done

--- Kandane algorithm.py
+++ Kandane algorithm.py
@@ -1,9 +1,17 @@
-def maxSubarraySum(Arr, N):
-    max_ending_here = Arr[0]
-    max_so_far = Arr[0]
+class Solution:
+    def maxSubArraySum(self, arr, N):
+        maxx = arr[0]
+        cur = 0
 
-    for i in range(1, N):
-        max_ending_here = max(Arr[i], max_ending_here + Arr[i])
-        max_so_far = max(max_so_far, max_ending_here)
+        for i in arr:
+            if cur < 0:
+                cur = 0
+            cur += i
+            maxx = max(maxx, cur)
+        return maxx
 
-    return max_so_far
+
+n = int(input())
+arr = [int(x) for x in input().strip().split()]
+ob = Solution()
+print(ob.maxSubArraySum(arr, n))
