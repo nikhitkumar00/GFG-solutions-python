a0e40858e4bf9770091b8ea932cc9ae160e60a18
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-09-18 09:08:09 +0530

    done

--- Longest valid Parenthesis.py
+++ Longest valid Parenthesis.py
@@ -2,18 +2,18 @@ class Solution:
     def maxLength(self, S):
         stack = [-1]
         maxx = 0
-        for i, num in enumerate(S):
+        for index, num in enumerate(S):
             if num == "(":
-                stack.append(i)
+                stack.append(index)
             else:
                 stack.pop()
                 if stack:
-                    maxx = max(maxx, i - stack[-1])
+                    maxx = max(maxx, index - stack[-1])
                 else:
-                    stack.append(i)
+                    stack.append(index)
         return maxx
 
 
-S = input()
+S = ")()()))("
 ob = Solution()
 print(ob.maxLength(S))
