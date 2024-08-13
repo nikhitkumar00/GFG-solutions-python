0aeb2ebd7dd92fc7c9288af778312e7fb7d73386
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-08-13 22:22:15 +0530

    done

---
+++ Square root of a number.py
@@ -0,0 +1,29 @@
+import math
+
+class Solution:
+    def floorSqrt(self, n): 
+        return math.floor(math.sqrt(n))
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        x = int(input())
+
+        print(Solution().floorSqrt(x))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
